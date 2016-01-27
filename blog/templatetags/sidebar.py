12db63a18a78be50fc2b8c7e8fbdb5b1d166ead1
dengshilong <dengshilong1988@gmail.com>
2016-01-27 18:12:50 +0800

    add sidebar tag

--- blog/templatetags/sidebar.py
+++ blog/templatetags/sidebar.py
@@ -1,6 +1,6 @@
 from django import template
 from markdown import markdown
-from blog.models import Post,Category,Link
+from blog.models import Post,Category,Link,Tag
 register = template.Library()
 @register.inclusion_tag('blog/archive_list.html', takes_context = True)
 def get_archive(context):
@@ -11,3 +11,6 @@ def get_categories(context):
 @register.inclusion_tag('blog/link_list.html', takes_context = True)
 def get_links(context):
     return {'links': Link.objects.all()}
+@register.inclusion_tag('blog/tag_list.html', takes_context = True)
+def get_tags(context):
+    return {'tags': Tag.objects.all()}
