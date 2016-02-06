975971720648b74e5c59c30798e782514682e58d
dengshilong <dengshilong1988@gmail.com>
2016-02-06 22:20:53 +0800

    add hightlight

--- blog/templatetags/my_markdown.py
+++ blog/templatetags/my_markdown.py
@@ -3,4 +3,4 @@ from markdown import markdown
 register = template.Library()
 @register.filter(name='mark')
 def mark(value):
-    return markdown(value)
+    return markdown(value, extensions=['markdown.extensions.extra', 'markdown.extensions.codehilite'])
