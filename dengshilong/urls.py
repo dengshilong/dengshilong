fc7b5c261c2327c359c76f9d6eb2112fb196b635
dengshilong <dengshilong1988@gmail.com>
2016-02-10 19:09:05 +0800

    modify base.html

--- dengshilong/urls.py
+++ dengshilong/urls.py
@@ -17,8 +17,7 @@ from django.conf.urls import url,include
 from django.contrib import admin
 from django.conf.urls.static import static
 from django.conf import settings
-import settings
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^', include('blog.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] 
