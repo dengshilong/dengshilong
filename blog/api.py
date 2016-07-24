7930af4214814115183c2899b251be0b09b1b2b8
dengshilong <dengshilong1988@gmail.com>
2016-07-24 18:01:33 +0800

    fix(blog): optimize sql

--- blog/api.py
+++ blog/api.py
@@ -1,10 +1,11 @@
-from .models import Post
+#coding: utf-8
+from .models import Post, Category
 from .serializers import PostSerializer
 from django.http import Http404
-from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework import generics
+
 
 
 class PostListAPI(generics.ListAPIView):
@@ -13,19 +14,26 @@ class PostListAPI(generics.ListAPIView):
     """
     model = Post
     serializer_class = PostSerializer
+    pagination_by = 10
 
     def get_queryset(self):
         queryset = Post.objects.all().order_by('-publish_time')
+        queryset = queryset.prefetch_related('category')
         return queryset
 
-class PostDetailAPI(APIView):
+class PostAPI(generics.RetrieveUpdateAPIView):
     """
     List single post
     """
-    def get(self, request, *args, **kwargs):
-        post = Post.objects.filter(pk=kwargs['pk'])
-        serializer = PostSerializer(post, many=True)
-        return Response(serializer.data)
+    model = Post
+    serializer_class = PostSerializer
+    queryset = Post.objects.all()
+
+    def get_queryset(self):
+        queryset = super(PostAPI, self).get_queryset()
+        queryset = queryset.prefetch_related('category')
+        return queryset
+
 
 class CategoryAPI(generics.ListAPIView):
     """
@@ -35,10 +43,15 @@ class CategoryAPI(generics.ListAPIView):
     serializer_class = PostSerializer
 
     def get_queryset(self):
-        queryset = Post.objects.filter(category=self._category).order_by('-publish_time')
+        queryset = Post.objects.filter(category__in=self._category).order_by('-publish_time')
+        queryset = queryset.prefetch_related('category')
         return queryset
 
     def get(self, request, *args, **kwargs):
-        self._category = kwargs['category']
+        category = kwargs['category']
+        category = Category.objects.filter(name=category)
+        if not category.exists():
+            return Response({"detail": u'不存在此分类'}, status=status.HTTP_404_NOT_FOUND)
+        self._category = category
         return super(CategoryAPI, self).get(request, *args, **kwargs)
 
--- blog/serializers.py
+++ blog/serializers.py
@@ -1,6 +1,13 @@
 from rest_framework import serializers
-from .models import Post
+from .models import Post, Category
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+
 
 class PostSerializer(serializers.ModelSerializer):
+    category = CategorySerializer(many=True)
+
     class Meta:
         model = Post
